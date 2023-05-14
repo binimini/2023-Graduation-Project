a360429dc8e60c07b15f1b4be7e7eb2ea45b0de4
Subin Min <tnqls5417@naver.com>
2023-05-14 19:55:22 +0900

    feat: ChatGPT 실시간 통신을 위한 소켓 이벤트 구현

--- socket/src/socket.ts
+++ socket/src/socket.ts
@@ -1,11 +1,55 @@
-import {Socket} from "socket.io";
+import {Server} from "socket.io";
+// @ts-ignore
+import {ask, init} from "./chat-gpt.ts";
 
-const handler = (socket : Socket) => {
-    console.log(`socket ${socket.id} connected`);
+interface Rooms {
+    [key:string]: RoomInfo
+}
 
-    socket.on("disconnect", (reason) => {
-        console.log(`socket ${socket.id} disconnected due to ${reason}`);
+interface RoomInfo {
+    conversationId: string,
+    parentMessageId: string
+}
+
+
+let rooms: Rooms = {};
+
+const registerSocketHandlers = (io : Server) => {
+    io.on("connection",  (socket)=> {
+        console.log(`socket ${socket.id} connected.`);
+
+        socket.on('join', async (room:string) => {
+            socket.join(room);
+
+            if (rooms[room] == undefined) {
+                rooms[room] = await init();
+            }
+            console.log(`socket ${socket.id} joined room ${room}.`);
+        });
+
+        socket.on('question', async (room: string, text: string) => {
+            if (rooms[room] == undefined) return;
+            const handleProgress = (room:string, role:string, id:string, text:string) => {
+                io.to(room).emit('progress', { role, id, text })
+            };
+            const response = await ask(room, rooms[room], text, handleProgress);
+            rooms[room].parentMessageId = response.id;
+        });
+
+        socket.on('feedback', async (room: string, code: string) => {
+            if (rooms[room] == undefined) return;
+            const handleProgress = (room:string, role:string, id:string, text:string) => {
+                io.to(room).emit('progress', { role, id, text })
+            };
+            const response = await ask(room, rooms[room], code, handleProgress);
+            rooms[room].parentMessageId = response.id;
+        });
+
+        socket.on("disconnect", (reason) => {
+            console.log(`socket ${socket.id} disconnected due to ${reason}`);
+        });
     });
 }
 
-export default handler;
+export {RoomInfo}
+export default registerSocketHandlers;
