d2a3fbb5ad3c98570fa5f63cb5bfd4373f7931c6
Subin Min <tnqls5417@naver.com>
2023-05-21 19:51:14 +0900

    feat: 사용자 정보 연동 추가

--- ui/src/components/AlgoProblem/AlgoFilter.tsx
+++ ui/src/components/AlgoProblem/AlgoFilter.tsx
@@ -7,23 +7,38 @@ import Tabs from "@/components/_styled/Tabs";
 import { algoProbListState, algoProbLevelState } from "@/store/algoProbState";
 import axios from "axios";
 import { cursorTo } from "readline";
+import {toastMsgState} from "@/store/toastMsgState";
 
+interface UserInfo {
+    userId: string
+    solvedCount: number;
+    profileImageUrl: string;
+    level: {
+        name: string
+    }
+}
 const AlgoFilterContainer = () => {
+    // 0 : 사용자 정보, 1 : 알고리즘 필터, 2 : 알고리즘 검색
   const [tabNum, setTabNum] = useState(0);
   const [algoProbLevelList] = useRecoilState(algoProbLevelState);
   const [, setAlgoProblemList] = useRecoilState(algoProbListState);
-  const resetAlgoProblemList = useResetRecoilState(algoProbListState);
+  const [, setToastObj] = useRecoilState(toastMsgState);
+  const [levelFilter, setLevelFilter] = useState("");
+  const [problemNum, setProblemNum] = useState("");
+  const [accountId, setAccountId] = useState("");
+  const [userInfo, setUserInfo] = useState<UserInfo>();
 
-  const saveSearchedProbList = () => {};
-
+  console.log(userInfo)
   const onSearch = () => {
-    console.log(problemNum, levelFilter);
-
     const url =
       tabNum == 0
-        ? `/api/problems/random?standard=level&id=${levelFilter}`
-        : `/api/problems?number=${problemNum}`;
-    console.log(url);
+        ? `/api/problems/user/${accountId}` :
+          tabNum==1 ? `/api/problems/random?standard=level&id=${levelFilter}` : `/api/problems?number=${problemNum}`;
+
+    if (tabNum == 0 && accountId=="") {
+        setToastObj({show:true, msg: '사용자 아이디를 정확히 입력해주세요!'})
+        return
+    }
 
     axios
       .get("http://localhost:8080" + url)
@@ -31,11 +46,15 @@ const AlgoFilterContainer = () => {
         const { data } = res;
 
         if (data == null) {
-          setAlgoProblemList({ list: [], length: 0, error: true });
+            if (tabNum==0) {
+                setUserInfo({});
+            }
+            else setAlgoProblemList({ list: [], length: 0, error: true });
           return;
         }
         tabNum == 0
-          ? setAlgoProblemList({
+            ? setUserInfo(data) : tabNum == 1
+            ? setAlgoProblemList({
               list: data,
               length: data.length,
               error: false,
@@ -47,18 +66,41 @@ const AlgoFilterContainer = () => {
       });
   };
 
-  const [levelFilter, setLevelFilter] = useState("");
-  const [problemNum, setProblemNum] = useState("");
 
   return (
     <>
       <Tabs
-        list={["필터검색", "번호검색"]}
+        list={["사용자정보","필터검색", "번호검색"]}
         tabNum={tabNum}
         setTabNum={setTabNum}
       />
       <ContentDiv>
         {tabNum == 0 ? (
+            <div className="flex justify-start h-[full-20px] w-full">
+                <div className="w-full h-[full-5px]">
+                <InputBox className="h-[20%]"
+                    placeholder="아이디를 입력하세요"
+                    label="사용자 아이디"
+                    setInput={setAccountId}
+                />
+                {userInfo==null ? null :
+                <div className="w-full h-[70%] flex flex-row justify-around mx-4">
+                    <img  className="w-[20%] h-fit text-xs font-bold" src={userInfo.profileImageUrl} alt="프로필"/>
+                    <div className="w-[70%] h-auto">
+                        <div className="text-xs font-bold flex flex-row h-[50%]">
+                            <div className="w-[40%]">해결한 문제 수</div>
+                            <div className="dark-1 w-[55%] text-center rounded-[10px] h-fit">{userInfo.solvedCount} </div>
+                        </div>
+                        <div className="text-xs font-bold flex flex-row h-[50%]">
+                            <div className="w-[40%]">티어</div>
+                            <div className="dark-1 w-[55%] text-center rounded-[10px] h-fit">{userInfo.level?.name ?? "None"}</div>
+                        </div>
+                    </div>
+                </div>
+                }
+                </div>
+            </div>) :
+            tabNum == 1 ? (
           <div className="flex justify-start">
             <SelectBox
               options={algoProbLevelList.list}
